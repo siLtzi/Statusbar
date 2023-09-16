9641803b9adb902e11632a9bb804556bd2653d87
siLtzi <siltzuri@gmail.com>
2023-09-16 12:07:05 +0300

    valmis

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform } from 'react-native';
+import Constants from 'expo-constants';
 
 export default function App() {
   return (
@@ -12,9 +13,6 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? Constants.statusBarHeight : 0,
   },
 });
